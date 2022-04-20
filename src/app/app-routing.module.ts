9651eb656ac4e6c1a87d1209e9feaec4dce7820c
Enos-Vinicius <enosviniciusda@gmail.com>
2022-04-20 12:10:22 -0300

    concluído mobile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
-    data: {
-      title: "Home"
-    }
+    data: {title: "Home"}
   },
 ];
 
